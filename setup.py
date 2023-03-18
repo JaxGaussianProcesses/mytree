7d18e28d2da40c491ee943fb9c492b627f72df27
Daniel Dodd <daniel_dodd@icloud.com>
2023-03-18 14:20:44 +0000

    Update.

---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+NAME = "mytree"
+
+CLASSIFIERS = [
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: Apache License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+]
+
+INSTALL_REQUIRES = [
+    "jax>=0.4.1",
+    "jaxlib>=0.4.1",
+    "simple-pytree==0.1.2",
+]
+
+EXTRA_REQUIRE = {
+    "dev": [
+        "pytest",
+        "pre-commit",
+        "pytest-cov",
+    ],
+}
+
+GLOBAL_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+def read(*local_path: str) -> str:
+    """Read a file, given a local path.
+
+    Args:
+        *local_path (str): The local path to the file.
+
+    Returns:
+        str: The contents of the file.
+    """
+    with codecs.open(os.path.join(GLOBAL_PATH, *local_path), "rb", "utf-8") as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        version="0.1.0",
+        author="Daniel Dodd",
+        author_email="daniel_dodd@icloud.com",
+        license="MIT",
+        description="Module pytrees that cleanly handle parameter trainability and transformations for JAX models.",
+        long_description=read("README.md"),
+        long_description_content_type="text/markdown",
+        packages=find_packages(".", exclude=["tests"]),
+        package_data={NAME: ["py.typed"]},
+        include_package_data=True,
+        python_requires=">=3.9",
+        install_requires=INSTALL_REQUIRES,
+        extras_require=EXTRA_REQUIRE,
+        zip_safe=True,
+    )
